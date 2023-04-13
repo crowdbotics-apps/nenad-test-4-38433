3e7202e0d8ab6a4b7f2396f1ef6e84d441c6e7f8
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-13 15:08:39 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled23/index.js
+++ screens/Untitled23/index.js
@@ -1,25 +1,35 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const PushNotificationScreen = () => {
-  return <View style={styles.container}>
+  const navigation = useNavigation();
+  return <Pressable onPress={() => {
+    navigation.navigate("aboutTheApp");
+  }}><View style={styles.container}>
       <Image source={{
-      uri: 'https://dummyimage.com/300x300/000/fff'
-    }} style={styles.image} />
+        uri: 'https://dummyimage.com/300x300/000/fff'
+      }} style={styles.image} />
       <Text style={styles.title}>New Notification</Text>
       <Text style={styles.message}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
         justo vel massa malesuada, vel bibendum velit bibendum.
       </Text>
-    </View>;
+    </View></Pressable>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: 314,
+    height: 328,
+    position: "absolute",
+    top: -20,
+    left: 21
   },
   image: {
     width: 150,
